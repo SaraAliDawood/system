d8dcc6059d7eda7630de3823e295d5e2567083c4
Sara Ali Dawood <saradawood605@gmail.com>
2024-10-19 11:51:03 +0300

    Merge branch 'master' into login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import SideBar from "./components/SideBar";
-import MainDashboard from "./components/UsersDashboard/MainDashboard";
 import LoginForm from "./components/LoginForm/LoginForm";
+import MainDashboard from "./components/UsersDashboard/MainDashboard";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="px-1 font-Poppins flex">
-      <SideBar />
-      <MainDashboard />
-      <LoginForm />
+      <Router>
+      <Routes>
+        
+      <Route path="/" element={
+        <>
+        <SideBar />
+         <LoginForm />
+        </>
+       
+        } />
+        <Route path="/dashboard" element={<MainDashboard />} />
+        {/* Other routes */}
+      </Routes>
+    </Router>
+    
+      
+      
     </div>
   );
 }
