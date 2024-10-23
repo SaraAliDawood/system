96698d01af9e45263925a800b784a219b4b0a7dd
SaraAliDawood <saradawood605@gmail.com>
2024-10-23 15:35:14 +0300

    lastLogin

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-import SideBar from "./components/SideBar";
-import LoginForm from "./components/LoginForm/LoginForm";
-import MainDashboard from "./components/UsersDashboard/MainDashboard";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminSignIn from './components/LoginForm/AdminSignIn'; // Your existing admin sign-in
+import UserSignIn from './components/LoginForm/UserSignIn'; // The new user sign-in component
+import MainDashboard from './components/UsersDashboard/MainDashboard';
+
 function App() {
   return (
-    <div className="px-1 font-Poppins flex">
-      <Router>
+    <Router>
       <Routes>
-        
-      <Route path="/" element={
-        <>
-        
-         <LoginForm />
-        </>
-       
-        } />
-        <Route path="/dashboard" element={<MainDashboard />} />
-        {/* Other routes */}
+        <Route path="/" element={<AdminSignIn />} />
+        <Route path="/user-login" element={<UserSignIn />} /> {/* New user login route */}
+        <Route path='/admin-dashboard' element={<MainDashboard />} />
       </Routes>
     </Router>
-    
-      
-      
-    </div>
   );
 }
 
 export default App;
+
